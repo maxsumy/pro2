b542dbafd8631de66a014c76aca8457ac4966521
maxsumy <grenki2@gmail.com>
2020-05-23 19:09:48 +0300

    start

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -58,6 +58,80 @@ const Start = ()=>{
                 </div>
             </div>
 
+            <div className="section3">
+                <div className="section3-left">
+                    <img src={require("./images/pets.png")} alt="pets"/>
+                </div>
+                <div className="section3-right">
+                    <p className="section3-title">Here is collected everything that your pet needs:</p>
+                    <ul>
+                        <li className="li1">professional veterinarian tips;</li>
+                        <li className="li2">useful information about education and care;</li>
+                        <li className="li3">fostering home search;</li>
+                        <li className="li4">information about pet-sitting and walking service;</li>
+                        <li className="li5">and of course, great communication with new </li>
+                        <li >friends in your social network!</li>
+
+                    </ul>
+
+                </div>
+            </div>
+
+            <div className="section4">
+                <div className="container-section4">
+                    <div className="section4-left">
+                        <div>Coming soon</div>
+                    </div>
+                    <div className="section4-middle">
+                        <p>We are planing to open a new service,</p>
+                        <p>where your cats and dogs can find their love!</p>
+
+                    </div>
+                    <div className="section4-right">
+                        <img src={require("./images/heart-regular.svg")} alt="heart" className="heart"/>
+                        LOVE
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="footer">
+                <div className="container-section4">
+                    <div className="footer-left">
+                        <a href="#"><img src={require("./images/propets_logo.svg")} alt="logo" className="footer-logo"/></a>
+                    </div>
+                    <div className="footer-middle">
+                        <div className="footer-middle-icons">
+                            <a href="#"><img src={require("./images/facebook-brands.svg")} alt="facebook-logo" className="footer-face"/></a>
+                            <a href="#"><img src={require("./images/instagram-brands.svg")} alt="instagram-logo" className="footer-inst"/></a>
+                        </div>
+                        <div className="footer-middle-text">
+                            <p>1600 Amphitheatre Pkwy</p>
+                            <p>Mountain View, CA 94043, USA</p>
+                        </div>
+
+                    </div>
+                    <div className="footer-right">
+                        <div className="footer-right-col1">
+                            <ul>
+                                <li className="fli1"><a href="">Lost</a></li>
+                                <li className="fli2"><a href="">Found</a></li>
+                                <li className="fli3"><a href="">VetHelp</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-right-col2">
+                            <ul>
+                                <li className="fli4"><a href="">Hotels</a></li>
+                                <li className="fli5"><a href="">Walking</a></li>
+                                <li className="fli6"><a href="">Fostering</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
 
         </div>
         
